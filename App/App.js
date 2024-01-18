8a6d05d464ad00fde41b0ff6ae81e02584e74573
DEBJYOTIRUMAN <debjyotiruman@gmail.com>
2024-01-19 02:43:49 +0530

    Minor Bug Fixed

--- App/App.js
+++ App/App.js
@@ -13,6 +13,7 @@ export default function App() {
   }
   LogBox.ignoreLogs(["Setting a timer for a long period of time"]);
   LogBox.ignoreLogs(["Can't perform a React state update on an unmounted component."]);
+  LogBox.ignoreAllLogs();
   typography();
   return <RootNavigation />;
 }
